9f993310740cc91d98788010c447783b9850ca4f
MatYGVZ <jesusprogrammignacc17@gmail.com>
2022-05-07 19:22:54 -0400

    basic functionality done

--- script.js
+++ script.js
@@ -1,32 +1,133 @@
+/* constants */
 const screen = document.getElementById("screen");
-const buttonsParent = document.getElementById("buttons")
 const buttons = document.querySelectorAll(".grid-item")
+const numbers = document.querySelectorAll(".number")
+const operators = document.querySelectorAll(".operator")
+const dot = document.querySelector(".dot")
+const equal = document.getElementById("equal")
 
+/* mathematical operations functions */
 function add(a,b) {
-    return a + b;
+    let result = a + b;
+    let rounded = result.toFixed(2)
+    screen.innerHTML = rounded
+    finalValue = rounded;
+    return rounded;
 }
 
 function subtract(a,b) {
-    return a - b;
+    let result = a - b;
+    let rounded = result.toFixed(2)
+    screen.innerHTML = rounded
+    return rounded;
 }
 
 function multiply(a,b) {
-    return a * b;
+    let result = a * b;
+    let rounded = result.toFixed(2)
+    screen.innerHTML = rounded
+    return rounded;
 }
 
 function divide(a,b) {
-    return a / b
+    let result = a / b;
+    let rounded = result.toFixed(2)
+    screen.innerHTML = rounded
+    return rounded;
 }
 
-function operate() {
-    subtract(10,20)
+/* operate function */
+function operate(a,b) {
+    let firstValueNumber = Number(a);
+    let secondValueNumber = Number(b);
+    if (operatorValue === "+") {
+        add(firstValueNumber,secondValueNumber);
+    }
+    else if (operatorValue === "-") {
+        subtract(firstValueNumber,secondValueNumber);
+    }
+    else if (operatorValue === "*") {
+        multiply(firstValueNumber,secondValueNumber);
+    }
+    else {
+        divide(firstValueNumber,secondValueNumber);
+    }
 }
 
+/* variables */
 let firstValue = "";
+let operatorValue = "";
+let secondValue = "";
+let finalValue = "";
 
-buttons.forEach(button => {
-    button.addEventListener("click", () => {
-        screen.innerHTML += button.value
-        firstValue += button.value
+/* numbers EvenListener */
+numbers.forEach(number => {
+    number.addEventListener("click", () => {
+        if (screen.textContent === "ERROR") {
+            screen.innerHTML = "";
+            screen.innerHTML += number.value;
+            firstValue += number.value;
+        }
+        else if (firstValue !== "" && operatorValue !== "") {
+            if (secondValue === "") {
+                screen.innerHTML = "";
+                screen.innerHTML += number.value;
+                secondValue += number.value;
+            }
+            else {
+                screen.innerHTML += number.value;
+                secondValue += number.value;
+            }
+        }
+        else {
+            screen.innerHTML += number.value;
+            firstValue += number.value;
+        }
     })
 });
+
+/* mathematical operators EventListener */
+operators.forEach(operator => {
+    operator.addEventListener("click", () => {
+        if (finalValue !== "") {
+            firstValue = finalValue;
+            secondValue = ""
+            finalValue = ""
+            screen.innerHTML = operator.value;
+        }
+        if (firstValue === "") {
+            screen.innerHTML = "ERROR";
+        }
+        else {
+            screen.innerHTML = operator.value;
+            operatorValue = operator.value;
+        }
+    })
+})
+
+/* decimal EventListener */
+dot.addEventListener("click", () => {
+    if(firstValue.indexOf(".") < 1 && operatorValue === "") {
+        screen.innerHTML += dot.value;
+        firstValue += dot.value;
+        dot.disable = "true"
+    }
+    else if (operatorValue !== "" && secondValue === "") {
+        dot.disable = "true"
+    }
+    else if(secondValue.indexOf(".") < 1 && firstValue !== "" && operatorValue !== "") {
+        screen.innerHTML += dot.value;
+        secondValue += dot.value;
+        dot.disable = "true"
+    }
+})
+
+/* equal button EventListener */
+equal.addEventListener("click", () => {
+    if (finalValue !== "") {
+        operate(finalValue)
+    }
+    if (firstValue !== "" && secondValue !== "") {
+        operate(firstValue,secondValue)
+    }
+})
